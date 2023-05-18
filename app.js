0bfd955fe6bea8dba742f8dd47ee656e3664013c
Asya <pleyko.a@mail.ru>
2023-05-18 15:48:47 +0200

    проверка

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
-const auth = require('./middlewares/auth');
+const { celebrate, Joi } = require('celebrate');
+const { auth } = require('./middlewares/auth');
+const { createUser, login } = require('./controllers/users');
+const NotFoundError = require('./errors/NotFoundError');
 
 const { PORT = 3000 } = process.env;
 
@@ -15,10 +18,29 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
-
+const regex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_[\]+.~#?&[\]/=]*)$/;
 app.use(auth);
 app.use(router);
 app.use(errors());
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(regex),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Произошла ошибка: страницы не существует'));
+});
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,12 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
 const routerCard = require('./cards');
 const routerUser = require('./users');
 const NotFoundError = require('../errors/NotFoundError');
-const { login, createUser } = require('../controllers/users');
-const auth = require('../middlewares/auth');
 
-const regex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_[\]+.~#?&[\]/=]*)$/;
-
-router.post(
-  '/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  login,
-);
-
-router.post(
-  '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().regex(regex),
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  createUser,
-);
-
-router.use('/cards', routerCard, auth);
-router.use('/users', routerUser, auth);
-router.use('*', auth, (req, res, next) => {
-  next(new NotFoundError('Произошла ошибка: страницы не существует'));
+router.use('/cards', routerCard);
+router.use('/users', routerUser);
+router.use('*', (req, res, next) => {
+  next(new NotFoundError('Произошла ошибка: страница не найдена'));
 });
 
 module.exports = router;
