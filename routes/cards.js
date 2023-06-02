36cad112ae3f08bc993699e3792cb6022d945002
Asya <pleyko.a@mail.ru>
2023-06-02 04:20:11 +0200

    check zanovo

--- routes/cards.js
+++ routes/cards.js
@@ -1,7 +1,5 @@
 const cardRouter = require('express').Router();
 const { celebrate } = require('celebrate');
-const auth = require('../middlewares/auth');
-
 const {
   createCardValidation,
   deleteCardValidation,
@@ -17,19 +15,8 @@ const {
 
 cardRouter.get('/', getCards);
 cardRouter.post('/', celebrate(createCardValidation), createCard);
-
-cardRouter.delete(
-  '/:cardId',
-  celebrate(deleteCardValidation),
-  auth,
-  deleteCard,
-);
-
+cardRouter.delete('/:cardId', celebrate(deleteCardValidation), deleteCard);
 cardRouter.put('/:cardId/likes', celebrate(likeCardValidation), likeCard);
-cardRouter.delete(
-  '/:cardId/likes',
-  celebrate(likeCardValidation),
-  removeLikeCard,
-);
+cardRouter.delete('/:cardId/likes', celebrate(likeCardValidation), removeLikeCard);
 
 module.exports = cardRouter;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
+const { celebrate } = require('celebrate');
 const routerCard = require('./cards');
 const routerUser = require('./users');
+const { login, createUser } = require('../controllers/users');
+const auth = require('../middlewares/auth');
+const { signinValidation, signupValidation } = require('../utils/validation');
 
+router.use(auth);
 router.use('/cards', routerCard);
 router.use('/users', routerUser);
+router.post('/signin', celebrate(signinValidation), login);
+router.post('/signup', celebrate(signupValidation), createUser);
 
 module.exports = router;
