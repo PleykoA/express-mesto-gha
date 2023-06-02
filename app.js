b75997e1483e38c739c1c1609ccfa8946d0a356e
Asya <pleyko.a@mail.ru>
2023-06-02 15:02:33 +0200

    check 8

--- app.js
+++ app.js
@@ -2,25 +2,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/NotFoundError');
 const authRouter = require('./routes/auth');
 const router = require('./routes');
-const auth = require('./middlewares/auth');
-const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+}).then(() => console.log('DB is connected'))
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(requestLogger);
 app.use(cors);
+app.use(requestLogger);
+
 app.use(authRouter);
-app.use(auth);
+
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
--- utils/utils.js
+++ utils/utils.js
@@ -1,10 +1,11 @@
 const RegExp = /https?:\/\/(www\.)?[\w-@:%.\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([\w-.~:/[?%#@!\]$&'()*+,;=]*)/;
-
-const allowedCors = [
-  'http://pleykoa.nomoredomains.monster',
-  'https://pleykoa.nomoredomains.monster',
-  'localhost:3000',
-];
+const allowedCors = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
 module.exports = {
   RegExp,
