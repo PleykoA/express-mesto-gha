0743d98851cfd1653176ae02ecc26fdf93b1313a
Asya <pleyko.a@mail.ru>
2023-06-02 04:28:18 +0200

    откатила

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('./middlewares/cors');
 
 const NotFoundError = require('./errors/NotFoundError');
 
+const authRouter = require('./routes/auth');
 const router = require('./routes');
+const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -15,20 +19,26 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+app.use(cors);
+app.use(authRouter);
+app.use(auth);
 app.use(router);
-
-app.use((req, res, next) => {
-  next(new NotFoundError('Страница не найдена'));
-});
-
+app.use(errorLogger);
 app.use(errors());
 
+app.use('*', (req, res) => {
+  res.status(new NotFoundError('Такой страницы не существует'));
+});
+
 app.use((err, req, res, next) => {
+  console.log(err);
   const { statusCode = 500, message } = err;
 
   res.status(statusCode).send({
-    message: statusCode === 500 ? 'Ошибка на сервере' : message,
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
+
   next();
 });
 
---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const authRouter = require('express').Router();
+const { celebrate } = require('celebrate');
+const { login, createUser } = require('../controllers/users');
+const { signinValidation, signupValidation } = require('../utils/validation');
+
+authRouter.post('/signin', celebrate(signinValidation), login);
+authRouter.post('/signup', celebrate(signupValidation), createUser);
+
+module.exports = authRouter;
