dd4036fc5c76bd58181e3d7d9b34c624e439f508
Asya <pleyko.a@mail.ru>
2023-06-02 14:31:57 +0200

    check 7

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,87 +1,88 @@
 const Card = require('../models/card');
 const NotFoundError = require('../errors/NotFoundError');
-const BadRequestError = require('../errors/BadRequestError');
 const ForbiddenError = require('../errors/ForbiddenError');
+const { handleErrors } = require('../utils/handleErrors');
 
-const getCards = (req, res, next) => {
+const getCards = (req, res) => {
   Card.find({})
-    .populate(['owner', 'likes'])
-    .then((cards) => {
-      res.send({ data: cards });
-    })
-    .catch(next);
+    .populate([
+      { path: 'likes', model: 'user' },
+      { path: 'owner', model: 'user' },
+    ])
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => handleErrors(err, res));
 };
 
-const createCard = (req, res, next) => {
+const createCard = (req, res) => {
   const { name, link } = req.body;
+  const owner = req.user;
 
-  Card.create({ name, link, owner: req.user._id })
+  Card.create({ name, link, owner })
     .then((card) => card.populate('owner'))
-    .then((card) => {
-      res.send({ data: card });
-    })
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        throw new BadRequestError('Переданы некорректные данные при создании карточки');
-      } else {
-        next(err);
-      }
-    });
+    .then((card) => res.status(201).send({ data: card }))
+    .catch((err) => handleErrors(err, res));
 };
 
-const deleteCard = (req, res, next) => {
-  const { cardId } = req.params;
-  Card.findByIdAndRemove(cardId)
-    .orFail(() => {
-      throw new NotFoundError('Нет карточки по заданному id');
-    })
+const deleteCard = (req, res) => {
+  const _id = req.params.cardId;
+
+  Card.findOne({ _id })
+    .populate([
+      { path: 'owner', model: 'user' },
+    ])
     .then((card) => {
-      if (card.owner.toString() === req.user._id) {
-        res.send(card);
-      } else {
+      if (!card) {
+        throw new NotFoundError('Карточка уже удалена');
+      }
+      if (card.owner._id.toString() !== req.user._id.toString()) {
         throw new ForbiddenError('Нельзя удалить чужую карточку');
       }
+      Card.findByIdAndDelete({ _id })
+        .populate([
+          { path: 'owner', model: 'user' },
+        ])
+        .then((deletedCard) => { res.send({ data: deletedCard }); });
     })
-    .catch(next);
+    .catch((err) => handleErrors(err, res));
 };
 
-const likeCard = (req, res, next) => {
+const likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
+    .populate([
+      { path: 'likes', model: 'user' },
+      { path: 'owner', model: 'user' },
+    ])
     .then((card) => {
-      if (!card) {
-        throw new NotFoundError('Карточка с таким id не найдена');
+      if (card) {
+        res.send({ data: card });
+      } else {
+        throw new NotFoundError('Карточка не найдена');
       }
-      return res.status(200).send(card);
     })
-    .catch((error) => {
-      if (error.name === 'CastError') {
-        next(new BadRequestError('Некорректные данные'));
-      }
-      next(error);
-    });
+    .catch((err) => handleErrors(err, res));
 };
-const removeLikeCard = (req, res, next) => {
+const removeLikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
     { new: true },
   )
+    .populate([
+      { path: 'likes', model: 'user' },
+      { path: 'owner', model: 'user' },
+    ])
     .then((card) => {
-      if (!card) {
-        throw new NotFoundError('Карточка с таким id не найдена');
+      if (card) {
+        res.send({ data: card });
+      } else {
+        throw new NotFoundError('Карточка не найдена');
       }
-      return res.status(200).send(card);
     })
-    .catch((error) => {
-      if (error.name === 'CastError') {
-        return next(new BadRequestError('Некорректные данные'));
-      }
-      return next(error);
-    });
+    .catch((err) => handleErrors(err, res));
 };
 module.exports = {
   getCards,
---
+++ utils/handleErrors.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const ForbiddenError = require('../errors/ForbiddenError');
+const NotFoundError = require('../errors/NotFoundError');
+const AuthorizationError = require('../errors/AuthorizationError');
+const BadRequestError = require('../errors/BadRequestError');
+
+const { CastError, ValidationError } = mongoose.Error;
+
+const handleErrors = (error, res) => {
+  if (error.code === 11000) {
+    return res.status(201).send({ message: 'Пользователь с данным email уже существует' });
+  }
+  if (error instanceof NotFoundError
+    || error instanceof AuthorizationError
+    || error instanceof ForbiddenError) {
+    const { message } = error;
+    return res.status(error.statusCode).send({ message });
+  }
+  if (error instanceof CastError || error instanceof ValidationError) {
+    return res.status(BadRequestError).send({ message: 'Переданы некорректные данные' });
+  }
+  return res.status(500).send({ message: 'На сервере произошла ошибка' });
+};
+
+module.exports = handleErrors;
