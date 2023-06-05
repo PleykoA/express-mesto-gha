9aa24bfe3161045e92e0b945fad73ce4cb8ae577
Asya <pleyko.a@mail.ru>
2023-06-05 18:21:36 +0200

    check 05/06 one

--- utils/utils.js
+++ utils/utils.js
@@ -1,12 +1,6 @@
 const RegExp = /https?:\/\/(www\.)?[\w-@:%.\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([\w-.~:/[?%#@!\]$&'()*+,;=]*)/;
 
-const cors = [
-  'http://pleykoa.nomoredomains.monster',
-  'https://pleykoa.nomoredomains.monster',
-  'localhost:3000',
-];
-
 module.exports = {
   RegExp,
-  cors,
+
 };
